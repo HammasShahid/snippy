b3ad941b75ef81247c4246e6870b23c23246f10b
hammas <dev.hammas@gmail.com>
2024-02-10 22:30:26 +0500

    style snippet show page

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import { db } from '@/db/';
 
@@ -8,6 +9,11 @@ interface Props {
 export default async function SnippetShowPage({ params: { id } }: Props) {
   const snippet = await db.snippet.findFirst({ where: { id: parseInt(id) } });
   if (!snippet) notFound();
-  
-  return <h1>{snippet.title}</h1>;
+
+  return (
+    <div className="flex flex-col gap-5">
+      <h1 className="text-2xl font-semibold">{snippet.title}</h1>
+      <pre className="bg-gray-300 p-3 rounded text-lg">{snippet.code}</pre>
+    </div>
+  );
 }
