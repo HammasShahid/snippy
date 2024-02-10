7b1873f4a2a2937a5b0873fc74b9e352611e532a
hammas <dev.hammas@gmail.com>
2024-02-10 16:21:08 +0500

    add a new snippet form

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,5 +1,20 @@
 export default function NewSnippetPage() {
   return (
-    <h1>New snippet</h1>
+    <>
+      <h1 className="text-4xl font-semibold tracking-wider mb-12">Add a new snippet</h1>
+
+      <form className="flex flex-col gap-5">
+        <div>
+          <label htmlFor="title" className="mr-5 text-lg">Title</label>
+          <input type="text" id="title" name="title" className="w-full border rounded px-4 py-1" />
+        </div>
+        <div>
+          <label htmlFor="code" className="mr-5 text-lg">Code</label>
+          <textarea id="code" name="code" className="w-full px-4 py-1 border rounded"></textarea>
+        </div>
+        
+        <button type="submit" className="bg-blue-200 w-full p-2 py-3 border rounded">Add</button>
+      </form>
+    </>
   )
 }
