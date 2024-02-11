181f5a2bf2f5f4193241974232a258c1dbe69653
hammas <dev.hammas@gmail.com>
2024-02-11 17:09:49 +0500

    fetch snippet in edit page

--- src/app/snippets/[id]/edit/page.tsx
+++ src/app/snippets/[id]/edit/page.tsx
@@ -1,9 +1,13 @@
+import { db } from '@/db';
+import { notFound } from 'next/navigation';
+
 interface Props {
   params: { id: string };
 }
 
-export default function SnippetEditPage({ params: { id } }: Props) {
-  return (
-    <div>Edit Snippet</div>
-  )
+export default async function SnippetEditPage({ params: { id } }: Props) {
+  const snippet = await db.snippet.findFirst({ where: { id: parseInt(id) } });
+  if (!snippet) notFound();
+
+  return <div>{snippet.title} </div>;
 }
